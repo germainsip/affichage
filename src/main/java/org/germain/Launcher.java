f0f98e9230a7c06a2f16c33c597299bc8d41a0d5
Germain SIPIERE <gsipiere@gmail.com>
2021-04-19 17:52:19 +0200

    choix du dossier où enregistrer le fichier

--- src/main/java/org/germain/Launcher.java
+++ src/main/java/org/germain/Launcher.java
@@ -1,7 +1,10 @@
 package org.germain;
 
+import java.util.Locale;
+
 public class Launcher {
     public static void main(String[] args) {
+        //Locale.setDefault("fr");
         Main.main(args);
     }
 }
